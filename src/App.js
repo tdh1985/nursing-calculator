b186b0c06315f277d531c230e3a841fb7bf1dd46
Tim Downey <timdowney@live.com.au>
2025-07-11 21:05:12 +1000

    Deploy React app to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NursingCalculator from './NursingCalculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NursingCalculator />
+    </div>
+  );
+}
+
+export default App;
